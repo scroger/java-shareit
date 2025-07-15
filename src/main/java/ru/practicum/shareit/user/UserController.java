c409477620e276df412307414555b0df9985e49a
Scroger <scroger@mail.ru>
2025-07-15 21:05:51 +0300

    Финальный проект спринта 14

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,54 @@
 package ru.practicum.shareit.user;
 
+import java.util.Collection;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ru.practicum.shareit.user.dto.CreateUserRequestDto;
+import ru.practicum.shareit.user.dto.UpdateUserRequestDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
+
 @RestController
-@RequestMapping(path = "/users")
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public Collection<User> getAll() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/{userId}")
+    public User getById(@PathVariable("userId") Long userId) {
+        return userService.getById(userId);
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody CreateUserRequestDto createUserRequestDto) {
+        return userService.create(createUserRequestDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public User update(@PathVariable("userId") Long userId,
+                       @Valid @RequestBody UpdateUserRequestDto updateUserRequestDto) {
+        return userService.update(userId, updateUserRequestDto);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void delete(@PathVariable("userId") Long userId) {
+        userService.delete(userId);
+    }
+
 }
